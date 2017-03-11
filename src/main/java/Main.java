3e28f66de23a4b89b90b93c031ba4f04c8b333c9
Janste <jansteczko@gmail.com>
2017-03-11 19:46:10 +0100

    Adding routes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,34 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        
+        post("/register", (req, res) -> {
+        	return "register";
+        });
+        
+        post("/register/:uuid", (req, res) -> {
+        	return "register uuid";
+        });
+        
+        post("/login", (req, res) -> {
+        	return "login";
+        });
+        
+        post("/request", (req, res) -> {
+        	return "post request";
+        });
+        
+        get("/request", (req, res) -> {
+        	return "get request";
+        });
+        
+        post("/request/:uuid", (req, res) -> {
+        	return "post request uuid";
+        });
+        
+        get("/request/:uuid", (req, res) -> {
+        	return "get request uuid";
+        });
+        
     }
 }
