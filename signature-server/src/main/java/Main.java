a3068c9afa74bb14f63d88a5810a96e7ca315004
Moomba <joe@rm-audio.com>
2017-04-07 21:39:56 +0200

    Handle CORS correctly

--- signature-server/src/main/java/Main.java
+++ signature-server/src/main/java/Main.java
@@ -1,6 +1,8 @@
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.port;
+import static spark.Spark.before;
+import static spark.Spark.options;
 
 import java.util.List;
 
@@ -18,6 +20,28 @@ public class Main {
     public static void main(String[] args) {
     	port(8001);
     	DatabaseHandler.getInstance();
+    	
+    	options("/*",
+    	        (request, response) -> {
+
+    	            String accessControlRequestHeaders = request
+    	                    .headers("Access-Control-Request-Headers");
+    	            if (accessControlRequestHeaders != null) {
+    	                response.header("Access-Control-Allow-Headers",
+    	                        accessControlRequestHeaders);
+    	            }
+
+    	            String accessControlRequestMethod = request
+    	                    .headers("Access-Control-Request-Method");
+    	            if (accessControlRequestMethod != null) {
+    	                response.header("Access-Control-Allow-Methods",
+    	                        accessControlRequestMethod);
+    	            }
+
+    	            return "OK";
+    	        });
+
+    	before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
         
         post("/api/register", (req, res) -> {
         	
